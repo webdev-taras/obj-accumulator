5de8a1fb56e1d95ca0985288d92cd4bbcfb3f475
webdev-taras <webdev.taras@gmail.com>
2019-05-17 18:49:01 +0300

    Accumulator tests

--- tests/accumulator.class.test.js
+++ tests/accumulator.class.test.js
@@ -1,15 +1,26 @@
 const test = require('ava')
-const { Accumulator } = require('../src/accumulator.class')
+const accumulatorFactory = require('../src/accumulator.class')
+const { Accumulator } = accumulatorFactory
 
 test.beforeEach(t => {
   t.context.item = new Accumulator()
 })
 
 test('new Accumulator() is instance of Accumulator', t => {
-  t.true(t.context.item instanceof Object)
-  t.true(t.context.item instanceof Accumulator)
+  const { item } = t.context
+  t.true(item instanceof Object)
+  t.true(item instanceof Accumulator)
 })
 
-test('new Accumulator() returns object with functions "list" as a propetry', t => {
-  t.is(typeof t.context.item, 'object')
+test('new Accumulator() returns object which props can not be deleted', t => {
+  const { item } = t.context
+  const prop = 'test'
+  item[prop] = 'some text'
+  t.false(delete item[prop])
+})
+
+test('accumulatorFactory() is instance of Accumulator', t => {
+  const item = accumulatorFactory()
+  t.true(item instanceof Object)
+  t.true(item instanceof Accumulator)
 })
